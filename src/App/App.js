c83f68d1be5c3575b23b775c6bad33519b716ab1
Marifel Angeles <mmarifel@gmail.com>
2019-04-18 04:19:40 -0500

    first commit

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  HashRouter as Router,
+  Route,
+} from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+}
+
+export default App;
